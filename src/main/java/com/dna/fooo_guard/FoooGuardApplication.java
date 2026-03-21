6799be0f122a39b4d8bd4232b016921ca7d6598f
DNA <sphz3813@gmail.com>
2026-03-21 22:29:03 +0900

    Init Project

---
+++ src/main/java/com/dna/fooo_guard/FoooGuardApplication.java
@@ -0,0 +1,13 @@
+package com.dna.fooo_guard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoooGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoooGuardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dna/fooo_guard/FoooGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dna.fooo_guard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoooGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
