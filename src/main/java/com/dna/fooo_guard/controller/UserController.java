f58007f7729a70cb5cd974ed3d0d8643a8623f3a
DNA B <sphz3813@gmail.com>
2026-03-28 20:58:11 +0900

    Add: User entity and BaseEntity

---
+++ src/main/java/com/dna/fooo_guard/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.dna.fooo_guard.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @GetMapping("/")
+    public String helloWorld() {
+        return "hello my world";
+    }
+    
+}
--- src/main/java/com/dna/fooo_guard/entity/User.java
+++ src/main/java/com/dna/fooo_guard/entity/User.java
@@ -12,15 +12,21 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED) // JPA를 위한 기본 생성자
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "users", comment = "사용자 정보 테이블")
-public class User {
+public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false, comment = "사용자 고유 고유 PK (Auto Increment)")
+    @Column(name = "id", nullable = false, comment = "사용자 PK (Auto Increment)")
     private Long id;
 
-    @Column(name = "user_id", nullable = false, length = 50, unique = true, comment = "사용자 아이디 (로그인 시 사용)")
-    private String userId;
+    @Column(name = "username", nullable = false, length = 50, unique = true, comment = "사용자 아이디 (로그인 시 사용)")
+    private String username;
+
+    @Column(name = "password", nullable = false, comment = "사용자 비밀번호 (암호화 저장)")
+    private String password;
+
+    @Column(name = "nickname", nullable = false, length = 50, unique = true, comment = "사용자 닉네임 (서비스 내에서 표시되는 이름)")
+    private String nickname;
 }
