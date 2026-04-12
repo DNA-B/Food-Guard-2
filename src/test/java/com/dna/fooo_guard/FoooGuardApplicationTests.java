5273b4185940a8a4b47496c0b2010714fc615541
DNA <sphz3813@gmail.com>
2026-04-12 16:19:46 +0900

    Add: Food test

--- src/test/java/com/dna/fooo_guard/FoooGuardApplicationTests.java
+++ src/test/java/com/dna/fooo_guard/FoooGuardApplicationTests.java
@@ -1,13 +1,8 @@
 package com.dna.fooo_guard;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FoooGuardApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
